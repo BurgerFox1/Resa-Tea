01e08bcb71faf466f7b5e7ebee2fa678cf51c990
BurgerFox1 <jeloaguilar1@gmail.com>
2022-12-04 06:50:57 +0800

    Big changes including hiding scrollbar
    - changes in script
    - added images
    - removed phone.js
    - added script.js
    - added meta "viewport"
    - changed div size and name
    - changed map
    - added facebook post
    - modal in script.js is unfinished
    - css in products is not fixed
    
    - etc. (baka may hindi ako nalagay)

---
+++ script.js
@@ -0,0 +1,29 @@
+//  scroll nav bar
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+//  modal image
+
+var modal = document.getElementById("image");
+
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
